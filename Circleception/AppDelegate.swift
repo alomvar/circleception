000778e88cd70ba974d0f4846969b4ff1d52faa3
Peter Horn <github@alomvar.com>
2016-01-15 18:06:29 +0100

    Initial Commit

---
+++ Circleception/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Circleception
+//
+//  Created by Peter Horn on 15.01.2016.
+//  Copyright © 2016 Peter Horn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Circleception/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Circleception
+//
+//  Created by Peter Horn on 15.01.2016.
+//  Copyright © 2016 Peter Horn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
